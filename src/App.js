e5d94411f1d4c07e3b84b930a23df4ec609214d8
2465622629 <2465622629@qq.com>
2023-05-21 17:26:24 +0800

    功能完成

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import './App.css';
-import {Hight} from "./menu/Hight";
-import { BrowserRouter as Router, Switch, Route } from 'react-router';
-import {Login} from "./login/Login"; // 引入路由
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import {Login} from "./login/Login";
+import {Hight} from "./menu/Hight"; // 引入路由
 
 
 
 const App = () => {
     return (
-        // <Router>
-        //     <div className="App">
-        //         <Switch>
-        //             <Route exact path="/" component={Hight} />
-        //         </Switch>
-        //     </div>
-        // </Router>
-        <div className="App">
-            <Login/>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route exact path="/login" element={<Login/>}/>
+                    <Route exact path="/" element={<Hight/>}/>
+                </Routes>
+            </div>
+        </Router>
     );
 };
 
--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -2,6 +2,7 @@ import './style.scss'
 
 import {Button, Input} from 'antd';
 import React from 'react';
+
 export function Login() {
     const [myKey, setMykey] = React.useState('') //卡密
     //发起登录请求
@@ -10,25 +11,24 @@ export function Login() {
         fetch('http://45.11.46.84/login.php', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
             },
             body: JSON.stringify({
                 kami: myKey,
             })
         }).then(res => res.json()).then(res => {
             console.log(`登录结果`, res)
-            if (res.code === 200) {
-                // localStorage.setItem('token', res.token)
-                // window.location.href = '/'
-                // console.log(res)
-                console.log(res)
+            if (res.status === 200) {
+                alert('登录成功')
+                window.location.href = '/'
             } else {
-                alert(res.msg)
+                alert('登录失败，请检查卡密是否正确')
             }
         }, err => {
             console.log(err)
         })
     }
+
     //处理登录
     function handleLogin() {
         if (myKey === '') {
@@ -37,6 +37,7 @@ export function Login() {
         }
         login()
     }
+
     return (
         <div className="login">
             <div className="contner">
@@ -45,10 +46,10 @@ export function Login() {
                 </div>
                 <div className="center_cont">
                     <div className="login_cont">
-                        <Input value={myKey} onChange={(e)=>{
+                        <Input value={myKey} onChange={(e) => {
                             setMykey(e.target.value)
-                        }} placeholder="请输入卡密"  allowClear />
-                        <Button size="large"  onClick={handleLogin}>登录</Button>
+                        }} placeholder="请输入卡密" allowClear/>
+                        <Button size="large" onClick={handleLogin}>登录</Button>
                     </div>
                 </div>
             </div>
