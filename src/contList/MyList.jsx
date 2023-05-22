5defba7b8a575bb3c3d577ce5ad0e6fca35a4a2c
2465622629 <2465622629@qq.com>
2023-05-22 23:13:00 +0800

    优化应用框的用户体验

--- src/contList/MyList.jsx
+++ src/contList/MyList.jsx
@@ -3,25 +3,21 @@ import {useEffect, useState} from "react";
 
 export function MyList(props){
     const { cont } = props;
-    const [isActive, setIsActive] = useState(false);
     const [content, setContent] = useState(cont);
 
     useEffect(() => {
         setContent(cont);
     }, [cont]);
 
-    const handleClick = () => {
-        setIsActive(!isActive);
-    };
+
 
     return (
-        <div className={isActive ? 'cont-main active' : 'cont-main'} onClick={handleClick}>
+        <div className="cont-main" onClick={() => props.onClick(content)}>
             <div className="cont-text">
                 <span>{cont}</span>
             </div>
             <div className="btns">
                 <button className="apply" onClick={() => props.onClick(content)}>应用</button>
-                <button className="copy">复制</button>
             </div>
         </div>
     )
