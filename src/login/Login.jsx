ac5f97b08b4a1e7c1b9f5f366d33c64d5103f45d
2465622629 <2465622629@qq.com>
2023-05-22 21:58:13 +0800

    添加通知气泡通知

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,35 +1,46 @@
 import './style.scss'
-import {Button, Input,message } from 'antd';
+import {Button, Input,notification} from 'antd';
 import React from 'react';
-import {api_address, proxy_address} from "../config/config";
-
+import {api_address} from "../config/config";
+import type { NotificationPlacement } from 'antd/es/notification/interface';
 export function Login() {
-    const [myKey, setMykey] = React.useState('') //卡密
-    const [messageApi, contextHolder] = message.useMessage();
+    const [myKey, setMyKey] = React.useState('') //卡密
+    const [api, contextHolder] = notification.useNotification(); //弹窗
+
+    //弹窗
+    const openNotification = (placement: NotificationPlacement,title,msg) => {
+        api.info({
+            message: title,
+            description:msg,
+            placement,
+        });
+    };
+
     //发起登录请求
     function login() {
         fetch(`${api_address}/login.php`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                // 'Access-Control-Allow-Origin': '*',
+
             },
             body: JSON.stringify({
                 kami: myKey,
             })
         }).then(res => res.json()).then(res => {
-            console.log(`登录结果`, res)
             if (res.status === 200) {
-                alert('登录成功')
-                console.log(res.token)
-                //设置本地cookie
-                document.cookie = `token=${res.token}`
-                // 将cookie 存到lockalStorage
-                localStorage.setItem('token', res.token)
-                //跳转到首页
-                window.location.href = `/index`
+                openNotification('top','登录成功','欢迎您')
+                setTimeout(() => {
+                    //设置本地cookie
+                    document.cookie = `token=${res.token}`
+                    // 将cookie 存到lockalStorage
+                    localStorage.setItem('token', res.token)
+                    //跳转到首页
+                    window.location.href = `/index`
+                }, 1000)
+
             } else {
-                alert('登录失败，请检查卡密是否正确')
+                openNotification('top','登录失败',res.msg)
             }
         }, err => {
             console.log(err)
@@ -37,28 +48,37 @@ export function Login() {
     }
 
     //弹窗
-    const openMessage = () => {
-        messageApi.info('This is a normal message');
-    };
+
     //处理登录
     function handleLogin() {
         if (myKey === '') {
-            openMessage()
-            return
+            openNotification('top','出错了','请输入卡密')
+        } else {
+            login()
         }
-        login()
+    }
+
+    function handleKeyDown(e) {
+        if (e.keyCode === 13) {
+            if (myKey === '') {
+                openNotification('top','出错了','请输入卡密')
+            } else {
+                login()
+            }
+        }
     }
 
     return (
         <div className="login">
+            {contextHolder}
             <div className="contner">
                 <div className="head_cont">
                     <h1>登录</h1>
                 </div>
                 <div className="center_cont">
                     <div className="login_cont">
-                        <Input value={myKey} onChange={(e) => {
-                            setMykey(e.target.value)
+                        <Input value={myKey} onKeyDown={handleKeyDown} onChange={(e) => {
+                            setMyKey(e.target.value)
                         }} placeholder="请输入卡密" allowClear/>
                         <Button type="primary" size="large" onClick={handleLogin}>登录</Button>
                     </div>
